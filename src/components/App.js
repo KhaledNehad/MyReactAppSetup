20abd863551735504311c4c712f8d4789c287c81
khalednehad <khalednehad@gmail.com>
2021-03-05 13:44:54 +0400

    My react app v1.0.0

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>Welcome Khaled</h1> <p>Let's do a beautiful App </p>
+    </div>
+  );
+};
+
+export default App;
